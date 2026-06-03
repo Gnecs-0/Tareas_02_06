4f85dcb85e8368f696a52dda5f426539db1f0e03
Gnecs-0 <gn.carvajal@duocuc.cl>
2026-06-02 22:46:43 -0400

    Finaliza ejercicio y pruebas

--- Tarea3.py
+++ Tarea3.py
@@ -7,10 +7,14 @@
 #Si intenta girar mas del dinero disponible, el programa debe mostrar un mensaje de error.
 #El menu debe repetirse hasta que el usuario decida salir.
 #Utilizar try-execpt para controlar ingresos incorrectos.
+import os, time
+os.system("cls")
 print("Bienvenido al cajero automático")
+time.sleep(2)
 saldo = 100000
 
 while True:
+    os.system("cls")
     print("""1. Consultar saldo
 2. depositar dinero
 3. Girar dinero
@@ -25,32 +29,44 @@ while True:
         except:
             print("Error! debe ingresar un núemero entero positivo.")
     if opc == 1:
+        os.system("cls")
         print("Su saldo es de: ", saldo)
+        time.sleep(2)
     elif opc == 2:
         while True:
+            os.system("cls")
             try:
                 depositar = int(input("Ingrese la cantidad a depositar: "))
                 if depositar <= 0:
                     print("Error! Ingrese una cantidad válida.")
+                    time.sleep(2)
                 else:
                     saldo += depositar
                     print("El nuevo saldo es: ", saldo)
                     break
             except:
                 print("Error! debe ingresar un número entero positivo.")
+                time.sleep(2)
     elif opc == 3:
         while True:
+            os.system("cls")
             try:
                 retirar = int(input("Cuánto dinero desea retirar?: "))
-                print("Error! debe ingresar un número entero positivo.")
-                if retirar > 0:
-                    break
+                if retirar > saldo:
+                    print("Error! No tiene suficiente saldo para retirar esa cantidad.")
+                    time.sleep(2)
+                    continue
                 elif retirar <= 0:
                     print("Error! Debe ingresar un número mayor a 0.")
-                elif retirar > saldo:
-                    print("Error! No tiene suficiente saldo para retirar esa cantidad.")
+                    time.sleep(2)
+                else:
+                    saldo -= retirar
+                    print("El nuevo saldo es: ", saldo)
+                    time.sleep(2)
+                    break
             except:
                 print("Error! debe ingresar un número entero positivo.")
+                time.sleep(2)
     elif opc == 4:
         print("Gracias por usar el cajero automático")
         break
