a0a94d23d800ecb662ad32afa645f5dc3ab6399b
Gnecs-0 <gn.carvajal@duocuc.cl>
2026-06-02 20:45:12 -0400

    Estructura inicial del programa

---
+++ tarea2.py
@@ -0,0 +1,9 @@
+#Ejercicio 2: Promedio de Notas
+
+#Solicite la cantidad de notas.
+#Ingrese cada nota utilizando un ciclo.
+#Calcule el promedio.
+#Indique si el estudiante aprueba o reprueba.
+#Utilice try/except para controlar errores.
+#Regla
+#Aprueba con promedio mayor o igual a 4.0.
