29bfc3a258a8a22ee968320d0e0dc18c9860a8b5
Gnecs-0 <gn.carvajal@duocuc.cl>
2026-06-02 20:09:46 -0400

    Incorporación de exepciones

--- Tarea1.py
+++ Tarea1.py
@@ -3,25 +3,39 @@ os.system ("cls")
 
 #estructura
 while True:
+    os.system ("cls")
     nombre = input("Ingrese nombre del producto: ").strip()
     if nombre.isalpha():
         break
     else:
         print("Error: El nombre del producto debe contener solo letras. Intente nuevamente.")
+        time.sleep(2)
 
 while True:
-    precio = int(input("Ingrese precio del producto: "))
-    if precio > 0:
-        break
-    else:
-        print("Error: El precio debe ser un número positivo. Intente nuevamente.")
+    os.system ("cls")
+    try:
+        precio = int(input("Ingrese precio del producto: "))
+        if precio > 0:
+            break
+        else:
+            print("Error: El precio debe ser un número positivo. Intente nuevamente.")
+            time.sleep(2)
+    except:
+        print("Error! Debe ingresar un número entero positivo para el precio.")
+        time.sleep(2)
 
 while True:
-    descuento = int(input("Ingrese descuento: "))
-    if 0 <= descuento <= 100:
-        break
-    else:
-        print("Error: El descuento debe ser un número entre 0 y 100. Intente nuevamente.")
+    os.system ("cls")
+    try:
+        descuento = int(input("Ingrese descuento: "))
+        if 0 <= descuento <= 100:
+            break
+        else:
+            print("Error: El descuento debe ser un número entre 0 y 100. Intente nuevamente.")
+            time.sleep(2)
+    except:
+        print("Error! Debe ingresar un número entero positivo entre 0 y 100 para el descuento.")
+        time.sleep(2)
 
 #calculo
 
