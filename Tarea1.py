7e3db7963f25147dc3f8672d341265f67896cd92
Gnecs-0 <gn.carvajal@duocuc.cl>
2026-06-02 20:02:28 -0400

    Validaciones agregadas

--- Tarea1.py
+++ Tarea1.py
@@ -2,16 +2,32 @@ import os, time
 os.system ("cls")
 
 #estructura
+while True:
+    nombre = input("Ingrese nombre del producto: ").strip()
+    if nombre.isalpha():
+        break
+    else:
+        print("Error: El nombre del producto debe contener solo letras. Intente nuevamente.")
 
-nombre = input("Ingrese nombre del producto: ")
-precio = int(input("Ingrese precio del producto: "))
-descuento = int(input("Ingrese descuento: "))
+while True:
+    precio = int(input("Ingrese precio del producto: "))
+    if precio > 0:
+        break
+    else:
+        print("Error: El precio debe ser un número positivo. Intente nuevamente.")
+
+while True:
+    descuento = int(input("Ingrese descuento: "))
+    if 0 <= descuento <= 100:
+        break
+    else:
+        print("Error: El descuento debe ser un número entre 0 y 100. Intente nuevamente.")
 
 #calculo
 
 precio_desc = precio*((100-descuento)/100)
 
-#Lo que se muestra}
+#Lo que se muestra
 
 print(f"""Producto: {nombre}
 Precio original: {precio}
