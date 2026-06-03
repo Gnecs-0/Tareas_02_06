1b29e61f57493f913ba300b4be4249ffdb4be5f1
Gnecs-0 <gn.carvajal@duocuc.cl>
2026-06-02 21:36:54 -0400

    Estructura del ejercicio

---
+++ Tarea3.py
@@ -0,0 +1,9 @@
+#Crear un programa de cajero automático que permita al usuario realizar las siguientes operaciones:
+#1. Consultar saldo
+#2. Retirar dinero
+#3. girar dinero
+#4. Salir
+#Reglas:
+#Si intenta girar mas del dinero disponible, el programa debe mostrar un mensaje de error.
+#El menu debe repetirse hasta que el usuario decida salir.
+#Utilizar try-execpt para controlar ingresos incorrectos.
