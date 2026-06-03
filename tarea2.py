8d40e5f627315300e689bd8add76b84263bd8811
Gnecs-0 <gn.carvajal@duocuc.cl>
2026-06-02 21:15:03 -0400

    Cálculos y validaciones

--- tarea2.py
+++ tarea2.py
@@ -7,7 +7,23 @@
 #Utilice try/except para controlar errores.
 #Regla
 #Aprueba con promedio mayor o igual a 4.0.
-
+acumulador_notas = 0
 cant_notas = int(input("Ingrese la cantidad de notas: "))
+if cant_notas <= 0:
+    print("La cantidad de notas debe ser mayor a cero.")
+
+
 for i in range(cant_notas):
     nota = float(input(f"ingrese la nota {i+1}: "))
+    if 1.0 <= nota <= 7.0:
+        acumulador_notas += nota
+
+promedio = acumulador_notas / cant_notas
+
+if promedio >= 4.0:
+    estado  = "Aprobado"
+else:
+    estado = "Reprobado"
+
+print(f"El promedio de las notas es: {promedio}")
+print(f"Estado: {estado}")
